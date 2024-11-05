0ac9d3f72fac4ecc135e931e513eee007f38855f
Ajay Rabidas <Ajay Rabidas@192.168.0.2>
2024-11-05 22:07:46 +0530

    Added add, update and delete operations

--- course-api/src/main/java/com/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/com/springbootstarter/topic/TopicService.java
@@ -3,9 +3,9 @@
  */
 package com.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -15,19 +15,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	// immuatable list
+//	private List<Topic> topics = Arrays.asList(
+//			new Topic("1", "English", "This is English"),
+//			new Topic("2", "Hindi", "This is Hindi"),
+//			new Topic("3", "Bengali", "This is Bengali")				
+//			);
+
+	// mutable list
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
 			new Topic("1", "English", "This is English"),
 			new Topic("2", "Hindi", "This is Hindi"),
 			new Topic("3", "Bengali", "This is Bengali")				
-			);
+			));
 
-	public List<Topic> getAllTopics(){
-		return topics;
-	};
-	
-	public Topic getTopicById(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-	}
-	
+			public List<Topic> getAllTopics() {
+				return topics;
+			};
+
+			public Topic getTopicById(String id) {
+				return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+			}
+
+			public void addTopic(Topic topic) {
+				topics.add(topic);
+			}
+
+			public void updateTopic(String id, Topic topic) {
+				for (int i = 0; i < topics.size(); i++) {
+					if (topics.get(i).getId().equals(id)) {
+						topics.set(i, topic);
+						return;
+					}
+				}
+			}
+
+			public void deleteTopic(String id) {
+				topics.removeIf(t -> t.getId().equals(id));
+			}
 	
 }
